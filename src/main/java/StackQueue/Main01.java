8bd5c0d004613eb40f99e62e8c519856b4ffad6e
noname_dev <noname_dev@naver.com>
2022-07-08 18:21:50 +0900

    Stack,Queue(올바른 괄호)문제

---
+++ src/main/java/StackQueue/Main01.java
@@ -0,0 +1,29 @@
+package StackQueue;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main01 {
+    public String solution(String str){
+        String answer = "NO";
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (chars[i] =='(') {
+                stack.push(chars[i]);
+            }else{
+                if (stack.isEmpty()) return "NO";
+                stack.pop();
+            }
+        }
+        if (stack.isEmpty()) answer="YES";
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main01 T = new Main01();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(T.solution(str));
+    }
+
+}
